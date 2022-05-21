8b56ac3a458c837f7b0166f421d06acb2e60840d
Usama-sys <usamaahmadscode@gmail.com>
2022-05-21 12:04:48 +0500

    delete spaces

--- routes/admin.php
+++ routes/admin.php
@@ -32,6 +32,4 @@ Route::middleware(['auth'])->group(function(){
     Route::get('dashboard', DashboardController::class)->name('dashboard');
     Route::resource('roles', RolesController::class);
 });
-
-
 require __DIR__.'/auth.php';
